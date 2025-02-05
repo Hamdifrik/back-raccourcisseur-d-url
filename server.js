4f0ad801fa2bff6da0e01da2a34889b30a0428dc
Hamdifrik <126709681+Hamdifrik@users.noreply.github.com>
2025-02-05 23:13:41 +0100

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,17 @@ app.use(express.json());
 app.use(cors());
 
 // Connexion à MongoDB
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  tls: true,
+  tlsInsecure: true // Permet d'éviter l'erreur SSL
+}).then(() => {
+  console.log('✅ Connecté à MongoDB');
+}).catch(err => {
+  console.error('❌ Erreur de connexion à MongoDB:', err);
+});
+
 
 
 // Modèle MongoDB
